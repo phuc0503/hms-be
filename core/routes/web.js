ef43500dad77ea50443d7fa83937fd4d36371209
Danny <chungthinh5900@gmail.com>
2024-04-21 16:47:05 +0700

    feat/get-patient-id

--- core/routes/web.js
+++ core/routes/web.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { getAllStaff } = require("../controllers/staffController");
-const { getAllPatient } = require("../controllers/patientController");
+const {
+  getAllPatient,
+  getPatientById,
+} = require("../controllers/patientController");
 
 const initWebRoutes = (app) => {
   router.get("/api/v1/get-all-staffs", getAllStaff);
-  router.get("/api/v1/get-all-patients", getAllPatient);
+  router.get("/api/v1/patient", getAllPatient);
+  router.get("/api/v1/patient/:patient_id", getPatientById);
   return app.use("/", router);
 };
 
