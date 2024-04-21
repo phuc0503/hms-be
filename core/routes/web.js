89d5e5fb05c47070fc51cccd5650255b9a458b2e
Danny <chungthinh5900@gmail.com>
2024-04-21 18:27:53 +0700

    merge branch main

--- core/routes/web.js
+++ core/routes/web.js
@@ -21,10 +21,6 @@ const initWebRoutes = (app) => {
   //patient
   router.get("/api/v1/patient", getAllPatient);
   router.get("/api/v1/patient/:patient_id", getPatientById);
-  return app.use("/", router);
-  router.get("/api/v1/get-all-staffs", getAllStaff);
-  router.get("/api/v1/patient", getAllPatient);
-  router.get("/api/v1/patient/:patient_id", getPatientById);
   router.get("/api/v1/patient/:patient_id/medicalRecord", getMedicalRecord);
   return app.use("/", router);
 };
