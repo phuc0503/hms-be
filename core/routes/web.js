79d24fb4b0d313f5d2952f102ad334acd534640c
Phuc <phucquangle0503@gmail.com>
2024-04-21 22:31:36 +0700

    commit-webjs

--- core/routes/web.js
+++ core/routes/web.js
@@ -2,28 +2,28 @@ const express = require("express");
 const router = express.Router();
 const { getAllStaff } = require("../controllers/staffController");
 const {
-  getAllPatient,
-  getPatientById,
-  getMedicalRecord,
+    getAllPatient,
+    getPatientById,
+    getMedicalRecord,
 } = require("../controllers/patientController");
 const { getAllDoctor, getDoctorById, createDoctor } = require("../controllers/doctorController");
 const initWebRoutes = (app) => {
-  //staf
-  router.get("/api/v1/staff", getAllStaff);
+    //staff
+    router.get("/api/v1/staff", getAllStaff);
 
-  //doctor
-  router.get("/api/v1/staff/doctor", getAllDoctor);
-  router.get("/api/v1/staff/doctor/:doctor_id", getDoctorById);
-  router.post("/api/v1/staff/doctor/create", createDoctor);
-  
-  //nurse
+    //doctor
+    router.get("/api/v1/staff/doctor", getAllDoctor);
+    router.get("/api/v1/staff/doctor/:doctor_id", getDoctorById);
+    router.post("/api/v1/staff/doctor/create", createDoctor);
 
-  //support staff
+    //nurse
 
-  //patient
-  router.get("/api/v1/patient", getAllPatient);
-  router.get("/api/v1/patient/:patient_id", getPatientById);
-  router.get("/api/v1/patient/:patient_id/medicalRecord", getMedicalRecord);
-  return app.use("/", router);
+    //support staff
+
+    //patient
+    router.get("/api/v1/patient", getAllPatient);
+    router.get("/api/v1/patient/:patient_id", getPatientById);
+    router.get("/api/v1/patient/:patient_id/medicalRecord", getMedicalRecord);
+    return app.use("/", router);
 };
 module.exports = initWebRoutes;
