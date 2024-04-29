d832e3d8cbc7e1e5711bf3907c481d098884cd48
Son <luongson159753@gmail.com>
2024-04-30 04:32:23 +0700

    re-update web.js

--- core/routes/web.js
+++ core/routes/web.js
@@ -7,6 +7,7 @@ const {
     getPatientById,
     getMedicalRecord,
     createPatient,
+    updatePatient
 } = require("../controllers/patientController");
 
 const {
@@ -28,7 +29,8 @@ const {
 const {
     getAllSupportStaff,
     getSupportStaffById,
-    createSupportStaff
+    createSupportStaff,
+    updateSupportStaff
 } = require("../controllers/supportStaffController");
 const { deleteToken } = require("firebase/messaging");
 const { deleteDoc } = require("firebase/firestore");
@@ -49,18 +51,20 @@ const initWebRoutes = (app) => {
     router.get("/api/v1/staff/nurses", getAllNurse);
     router.get("/api/v1/staff/nurses/:nurse_id", getNurseById);
     router.post("/api/v1/staff/nurses", createNurse);
-    // router.put("api/v1/staff/nurses/:nurse_id", updateNurse);
+    router.put("api/v1/staff/nurses/:nurse_id", updateNurse);
 
     //support staff
     router.get("/api/v1/staff/supportStaff", getAllSupportStaff);
     router.get("/api/v1/staff/supportStaff/:supportStaff_id", getSupportStaffById);
     router.post("/api/v1/staff/supportStaff", createSupportStaff);
+    router.put("/api/v1/staff/supportStaff/:supportStaff_id", updateSupportStaff);
 
     //patient
     router.get("/api/v1/patients", getAllPatient);
     router.get("/api/v1/patients/:patient_id", getPatientById);
     router.get("/api/v1/patients/:patient_id/medicalRecords", getMedicalRecord);
     router.post("/api/v1/patients", createPatient);
+    router.put("/api/v1/patients/:patient_id", updatePatient);
 
     return app.use("/", router);
 };
